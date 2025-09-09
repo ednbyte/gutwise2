dd38eb9118dc1c38f267d1a77e1f0f069498f8a9
ednbyte <emilyjohn2097@gmail.com>
2025-09-08 19:36:04 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -20,13 +20,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(
-    mongo_url,
-    tls=True,
-    tlsAllowInvalidCertificates=True,
-    serverSelectionTimeoutMS=30000,
-    connectTimeoutMS=30000,
-    socketTimeoutMS=30000,) 
+client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
 # Create the main app without a prefix
