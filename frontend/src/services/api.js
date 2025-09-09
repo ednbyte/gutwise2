ee43ca55408351f85411c4b617875d3ac342b217
ednbyte <emilyjohn2097@gmail.com>
2025-09-08 19:58:28 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API_BASE = `${BACKEND_URL}/api`;
+const API_BASE = '/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
